9f3368c4715b7054aa24ba2ade62b255d414a3ff
LEPAGNEUL Marc <marc@lepagneul.fr>
2024-12-17 15:07:45 +0100

    Générer la liste des codewords en symboles QPSK

---
+++ GenerateQPSKCodewords.m
@@ -0,0 +1,12 @@
+function QPSKCodewords = GenerateQPSKCodewords(G, A)
+    ListeCodeWords = GenerateListeCodeWords(G)
+    numCodeWords = size(ListeCodeWords, 1);
+    n_c = size(G, 2);  
+    
+    for i = 1:numCodeWords
+        codeword = ListeCodeWords(i, :);
+        
+        qpskSymbols = Bit2SymbolMappingQPSKGray(A, codeword);
+        QPSKCodewords(i, :) = qpskSymbols;
+    end
+end
