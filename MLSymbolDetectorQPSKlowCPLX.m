7ec9996a9b7248cf7c89e25b09c0d8fbd42eebd0
ErwanDE <62588973+ErwanDE@users.noreply.github.com>
2024-12-18 19:50:20 +0100

    Update MLSymbolDetectorQPSKlowCPLX.m
    commentaires

--- MLSymbolDetectorQPSKlowCPLX.m
+++ MLSymbolDetectorQPSKlowCPLX.m
@@ -3,18 +3,15 @@ function d_est = MLSymbolDetectorQPSKlowCPLX(A,z)
      a_kre = (A/sqrt(2)) * [-1; 1];
      a_kim = (A/sqrt(2)) * [-1i; 1i];
 
-% Initialiser les vecteurs im et re avec les mêmes dimensions que z
      im = zeros(1, length(z));
      re = zeros(1, length(z));
-
-     im(imag(z) > 0) = a_kim(2); % Si la partie imaginaire est positive
-     im(imag(z) <= 0) = a_kim(1); % Si la partie imaginaire est négative ou nulle
+     %detection en utilisant le symbole des parties imaginaires et reelles
+     im(imag(z) > 0) = a_kim(2);
+     im(imag(z) <= 0) = a_kim(1);
     
-     re(real(z) > 0) = a_kre(2); % Si la partie réelle est positive
-     re(real(z) <= 0) = a_kre(1); % Si la partie réelle est négative ou nulle    
+     re(real(z) > 0) = a_kre(2);
+     re(real(z) <= 0) = a_kre(1);
 
      d_est=re+im;
      d_est = d_est.';
-     
-
 end
