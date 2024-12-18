096283b37870da98408cdcd57bb9c58e47d9ef7e
ErwanDE <62588973+ErwanDE@users.noreply.github.com>
2024-12-18 19:42:21 +0100

    Update GenerateListeCodeWords.m
    retirer commentaire

--- GenerateListeCodeWords.m
+++ GenerateListeCodeWords.m
@@ -1,7 +1,6 @@
 function ListeCodeWord = GenerateListeCodeWords(G)
     k_c = size(G, 1);
     n_c = size(G, 2);
-    % Generate all possible binary messages
     messages = dec2bin(0:2^k_c-1)-48;
     ListeCodeWord = zeros(2^k_c, n_c);
     for i = 1:2^k_c
