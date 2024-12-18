75382350e5d1fcd94004e32845b97d9e98181cce
ErwanDE <62588973+ErwanDE@users.noreply.github.com>
2024-12-18 19:47:54 +0100

    Update IsErrorDetection.m
    commentaire

--- IsErrorDetection.m
+++ IsErrorDetection.m
@@ -1,5 +1,5 @@
 function error = IsErrorDetection(r, H)
     reshaped_r = reshape(r, size(H, 2), []).';
-    syndrome = mod(reshaped_r * H.', 2); % Calculate the syndrome
-    error = any(syndrome, 2); % If the syndrome is not all zeros, there's an error
+    syndrome = mod(reshaped_r * H.', 2); % syndrome
+    error = any(syndrome, 2);
 end
